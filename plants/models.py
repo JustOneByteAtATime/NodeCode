235eb4be8b5e27a8a8b3685d1adec794a0a43559
Jon Whittenburg <jon.whittenburg@gmail.com>
2021-11-09 17:30:58 -0700

    Commit with posts and images

--- plants/models.py
+++ plants/models.py
@@ -1,12 +1,21 @@
+from django.conf import settings
+from django.contrib.auth import get_user_model
 from django.db import models
+from django.urls import reverse
 
 class Post(models.Model):
-    title = models.CharField(max_length=200)
+    title = models.CharField(max_length=255)
+    body = models.TextField()
+    zipcode = models.CharField(max_length=5, null=True)
+    date = models.DateTimeField(auto_now_add=True)
+    image = models.ImageField(null=True, blank=True, upload_to="images/")
     author = models.ForeignKey(
-        'auth.User',
+        get_user_model(),
         on_delete=models.CASCADE,
     )
-    body = models.TextField()
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse('post_detail', args=[str(self.id)])
