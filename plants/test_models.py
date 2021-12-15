c08a87a463ee41dd477ab2344064436fef8891f2
Mike Nelson <michael1.nelson@umconnect.umt.edu>
2021-12-14 21:41:49 -0700

    Working model tests and other unit tests

--- plants/test_models.py
+++ plants/test_models.py
@@ -1,45 +1,51 @@
 from django.test import TestCase
-
-class NodeCodeTestCase(TestCase):
-    @classmethod
-    def setUpTestData(cls):
-        print("setUpTestData: Run once to set up non-modified data for all class methods.")
-        pass
-
-    def setUp(self):
-        print("setUp: Run once for every test method to setup clean data.")
-        pass
-
-    def test_false_is_false(self):
-        print("Method: test_false_is_false.")
-        self.assertFalse(False)
-
-    # This method is clearly designed to fail:
-##    def test_false_is_true(self):
-##        print("Method: test_false_is_true.")
-##        self.assertTrue(False)
-
-    def test_one_plus_one_equals_two(self):
-        print("Method: test_one_plus_one_equals_two.")
-        self.assertEqual(1 + 1, 2)
-
 from plants.models import Post
+import pytz
+from unittest import mock
+from django.contrib.auth.models import User
+from django.test import tag
+from django.conf import settings
+import os
+from PIL import Image
+from django.core.files.uploadedfile import SimpleUploadedFile
 
 class PostModelTest(TestCase):
-    @classmethod
-    def setUpTestData(cls):
+    def setUp(self):
+        self.temp_user = User.objects.create_user("TestUser")
         # Set up non-modified objects used by all test methods
-        Post.objects.create(title= 'Titan Arum', body= 'Totally kidding. We do not have corpse flower cuttings!', zipcode= '58720')
 
+    def tearDown(self):
+        User.objects.all().delete()
+
+        
+
+    @tag('model')
     def test_title_label(self):
-        post = Post.objects.get(id=1)
-        field_label = post._meta.get_field('title').verbose_name
-        self.assertEqual(field_label, 'instantiated (post)title')
+        post = Post.objects.create(title= "temp title", body="temp_body", zipcode="12345",author=self.temp_user)
+        self.assertEqual(post.title, "temp title")
 
     def test_title_max_length(self):
         post = Post.objects.get(id=1)
         max_length = post._meta.get_field('title').max_length
         self.assertEqual(max_length, 255)
+
+    @tag('model')
+    def test_post_record_create_without_image_will_return_default_image(self):
+        post = Post.objects.create(title= "temp title", body="temp_body", zipcode="12345",author=self.temp_user,)
+        self.assertEqual(post.image.name, 'default_plant.jpg')
+
+    @tag('model')
+    def test_post_record_create_without_image_will_get_default_image_file_from_configured_media_location(self):
+        post = Post.objects.create(title= "temp title", body="temp_body", zipcode="12345",author=self.temp_user,)
+        image_path = os.path.join(settings.MEDIA_ROOT, 'default_plant.jpg')
+        self.assertEqual(post.image.path, image_path)
+
+    @tag('model')
+    def test_post_record_create_with_image_will_create_image_file_under_images_folder_in_configured_media_location(self):
+        post = Post.objects.create(title= "temp title", body="temp_body", zipcode="12345",author=self.temp_user,image=SimpleUploadedFile('test_image.jpg', b''))
+        image_path = os.path.join(settings.MEDIA_ROOT, 'images', 'default_plant.jpg')
+        self.assertEqual(post.image.path, image_path)
+
 
     def test_body_label(self):
         post = Post.objects.get(id=1)
